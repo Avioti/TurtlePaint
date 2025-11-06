2d8f4e59fd04546f4b53383e4dc7a2245dd4cee0
Avi <95044844+Avioti@users.noreply.github.com>
2025-11-06 13:30:31 -0500

    square done

--- src/main/java/com/pluralsight/shapes/Square.java
+++ src/main/java/com/pluralsight/shapes/Square.java
@@ -1,9 +1,35 @@
 package com.pluralsight.shapes;
 
-public class Square extends Shape{
+import com.pluralsight.data.Turtle;
+import com.pluralsight.ui.utility.Inputs;
+
+import java.awt.*;
+
+import static com.pluralsight.ui.TurtlePainterApp.*;
+
+public class Square extends Shape {
+    private int sideLength;
+
+    public Square(Turtle turtle, Point location, String color, int border, int sideLength) {
+        super(turtle, location, color, border);
+        this.sideLength = sideLength;
+    }
 
     @Override
-    void paint() {
+    public void paint() {
+        turtle = new Turtle(world, -100, -100);
+        turtle.setPenWidth(border);
+        turtle.setColor(Inputs.parseColor(color));
+        turtle.penUp();
+        turtle.goTo(location);
+        turtle.penDown();
+        int sides = 4;
+        for (int i = 0; i < sides; i++) {
+            turtle.forward(sideLength);
+            turtle.turnRight(90);
+        }
+        homeScreen();
+        handleChoice();
 
     }
 }
