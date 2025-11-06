33de3663bb5148b789719c88d5a726c272a100f2
Avi <95044844+Avioti@users.noreply.github.com>
2025-11-06 13:30:49 -0500

    triangle done but not accurate

--- src/main/java/com/pluralsight/shapes/Triangle.java
+++ src/main/java/com/pluralsight/shapes/Triangle.java
@@ -1,9 +1,44 @@
 package com.pluralsight.shapes;
 
+import com.pluralsight.data.Turtle;
+import com.pluralsight.ui.utility.Inputs;
+
+import java.awt.*;
+
+import static com.pluralsight.ui.TurtlePainterApp.*;
+
 public class Triangle extends Shape{
 
+    public Triangle(Turtle turtle, Point location, String color, int border) {
+        super(turtle, location, color, border);
+    }
+
     @Override
-    void paint() {
+   public void paint() {
+        turtle = new Turtle(world,-100, -100);
+        int width = 200;
+        int height = 200;
+
+        // calculate the hypotenuse (diagonal)
+        // a2 + b2 = c2
+        double widthSquared = Math.pow(width, 2);
+        double heightSquared = Math.pow(height, 2);
+        double hypotenuse = Math.sqrt(widthSquared + heightSquared);
+
+
+        turtle.setPenWidth(border);
+        turtle.setColor(Inputs.parseColor(color));
+        turtle.penUp();
+        turtle.goTo(location);
+        turtle.penDown();
+
+        int sides = 3;
+        for (int i = 0; i < sides; i++) {
+            turtle.turnRight(120);
+            turtle.forward(hypotenuse);
+        }
+        homeScreen();
+        handleChoice();
 
     }
 }
